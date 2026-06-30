41ffdccd85b120a7c90331185b1c61c14416c9c1
Noel Chiasson <noel@discoverygarden.ca>
2026-06-30 09:17:36 -0300

    Updating script with additional tags.

--- scripts/add_labels.sh
+++ scripts/add_labels.sh
@@ -9,6 +9,7 @@ OWNER=${2}
 # Repository name
 REPO=${3}
 
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $(cat $TOKEN)" -d '{"name":"Major","description":"Backwards incompatible API changes.","color":"CC4E4E"}'
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $(cat $TOKEN)" -d '{"name":"Minor","description":"Added functionality that is backwards compatible.","color":"FFB24A"}'
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $(cat $TOKEN)" -d '{"name":"Patch","description":"Backwards compatible bug fixes.","color":"2766C6"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"Major","description":"Backwards incompatible API changes.","color":"CC4E4E"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"Minor","description":"Added functionality that is backwards compatible.","color":"FFB24A"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"Patch","description":"Backwards compatible bug fixes.","color":"2766C6"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"no-update","description":"No tag/release needs to be created","color":"BFDADC"}'
