d2af4acba7610d6326fbd3a064767ca189f0bf7b
Noel Chiasson <noel@discoverygarden.ca>
2026-06-30 09:20:18 -0300

    Fixing tag capitalization

--- scripts/add_labels.sh
+++ scripts/add_labels.sh
@@ -9,7 +9,7 @@ OWNER=${2}
 # Repository name
 REPO=${3}
 
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"Major","description":"Backwards incompatible API changes.","color":"CC4E4E"}'
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"Minor","description":"Added functionality that is backwards compatible.","color":"FFB24A"}'
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"Patch","description":"Backwards compatible bug fixes.","color":"2766C6"}'
-curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"no-update","description":"No tag/release needs to be created","color":"BFDADC"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"major","description":"Backwards incompatible API changes.","color":"CC4E4E"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"minor","description":"Added functionality that is backwards compatible.","color":"FFB24A"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"patch","description":"Backwards compatible bug fixes.","color":"2766C6"}'
+curl -f -X POST https://api.github.com/repos/$OWNER/$REPO/labels -H "Authorization: token $TOKEN" -d '{"name":"no-update","description":"No tag/release needs to be created.","color":"BFDADC"}'
